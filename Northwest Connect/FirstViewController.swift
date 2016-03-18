698f86afa78be43b0aa26789266470e68ef86e12
Kola,Harish <s525030@ch140021m.nwmissouri.edu>
2016-03-18 16:34:43 -0500

    Initial Commit

---
+++ Northwest Connect/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Northwest Connect
+//
+//  Created by Kola,Harish on 3/18/16.
+//  Copyright © 2016 Kola,Harish. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
