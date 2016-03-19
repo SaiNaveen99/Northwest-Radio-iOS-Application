64df206167b2eff079e04c38f911303217b2d4a4
Student <s525037@ch140020m.nwmissouri.edu>
2016-03-18 20:27:20 -0500

    Created web view and loaded the webpage or Radio page

--- Northwest Connect/RadioViewController.swift
+++ Northwest Connect/RadioViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 
-class RadioViewController: UIViewController {
+class RadioViewController: UIViewController,UIWebViewDelegate {
 
+    @IBOutlet weak var KRNWWV: UIWebView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         print("testing")
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //90.5
+        KRNWWV.delegate = self
+        //KRNWM.delegate = self
+        let url = NSURL(string: "http://www.kxcv.org/listenlive.htm")
+        let requestObj = NSURLRequest(URL: url!)
+        print(requestObj)
+        KRNWWV.loadRequest(requestObj)
+//        
+//        //KRNWM 88.9
+//        let url2 = NSURL(string: "http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=http://yp.shoutcast.com/sbin/tunein-station.pls?id=214270&amp;play_status=0")
+//        let requestObj1 = NSURLRequest(URL: url2!)
+//        KRNWWV.loadRequest(requestObj1)
+
     }
 
     override func didReceiveMemoryWarning() {
